96fe8c8ab347b86fac27f1798c1a38825d00639c
mangosax <mcguerty@att.net>
2023-09-06 13:11:52 -0500

    updates

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+
+        //Fields use camel casing
+        private double balanace = 1000;
+
+        //Property
+        //public double balance { get; set; }
+        
+
+        public void Deposit(double deposit)
+        {
+            balanace += deposit;
+        }
+
+        public double GetBalance()
+        {
+            //Console.WriteLine($"Your Balance is: {balanace}");
+            return balanace;
+        }
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace BankEncapsulation
+﻿
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Good Morning Mr. Bond!");
+
+
+
+            BankAccount bankAccount = new BankAccount();    
+
+            Console.WriteLine($"Your Balanace is {bankAccount.GetBalance()}");
+
+            Console.WriteLine($"Make a deposit, enter amount: ");
+            string? tmpInput = Console.ReadLine();
+            if ( tmpInput != null )
+            {
+                bankAccount.Deposit(int.Parse(tmpInput));
+                Console.WriteLine($"Thank your for your deposit of {int.Parse(tmpInput)}");
+            }
+            else
+            {
+                Console.WriteLine($"You entered an invalid amount...."); 
+            }
+            
+            Console.WriteLine($"Your Balanace is {bankAccount.GetBalance()}");
         }
     }
 }
